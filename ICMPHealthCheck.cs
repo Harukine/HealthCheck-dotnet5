1278066abbdeb78b9a96cb31b329dcc7dda306f5
Jacob <jacobhtnguyen@gmail.com>
2021-06-17 04:17:00 -0400

    created ICMPHealthCheck component

---
+++ ICMPHealthCheck.cs
@@ -0,0 +1,37 @@
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthCheck
+{
+    public class ICMPHealthCheck : ICMPHealthCheck
+    {
+        private readonly string Host = "www.does-not-exist.com";
+        private readonly int HealthyRoundtripTime = 300;
+
+        public async Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default)
+        {
+            try 
+            {
+                using var ping = new Ping();
+                var reply = await ping.SendPingAsync(Host);
+                switch (reply.Status)
+                {
+                    case IPStatus.Success:
+                        return (reply.RoundtripTime > HealthyRoundtripTime)
+                        ? HealthCheckResult.Degraded() : HealthCheckResult.Healthy();
+                    default:
+                        return HealthCheckResult.Unhealthy();
+                }
+            }
+            catch (Exception e)
+            {
+                return HealthCheckResult.Unhealthy();
+            }
+        }
+    }
+}
